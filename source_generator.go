11959c7a716443e0ec3adea7b3a9043376af52ca
General Kroll <generalkroll0@gmail.com>
2022-01-13 23:12:10 +1100

    works-but-oversized

--- source_generator.go
+++ source_generator.go
@@ -25,6 +25,14 @@ func (svc *Service) ToSourceFile(outFile string) error {
 		return err
 	}
 	cs := getSpewConfig()
+	// for k, v := range svc.Components.Schemas {
+	// 	sv := cs.Sdump(v.Value)
+	// 	f.Write([]byte(fmt.Sprintf("Schema_%s := %s\n\n", k, sv)))
+	// }
+	// for k, v := range svc.Components.Schemas {
+	// 	sv := cs.Sdump(v)
+	// 	f.Write([]byte(fmt.Sprintf("Schema_%s := %s\n\n", k, sv)))
+	// }
 	cs.Fdump(f, svc)
 	return nil
 }
