77bc27ebbe3a6513c7be2874e066dd3920e6c933
General Kroll <generalkroll0@gmail.com>
2022-05-23 11:57:43 +1000

    test-more-meaningful

--- openapistackql/operation_store_test.go
+++ openapistackql/operation_store_test.go
@@ -4,24 +4,16 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"path"
 	"strings"
 	"testing"
 
 	"github.com/getkin/kin-openapi/openapi3"
 	. "github.com/stackql/go-openapistackql/openapistackql"
-	"github.com/stackql/go-openapistackql/pkg/fileutil"
 
 	"github.com/stackql/go-openapistackql/test/pkg/testutil"
 
 	"gotest.tools/assert"
 )
-
-func getFileRoot(t *testing.T) string {
-	rv, err := fileutil.GetFilePathUnescapedFromRepositoryRoot(path.Join("test", "registry", "src"))
-	assert.NilError(t, err)
-	return rv
-}
 
 func TestPlaceholder(t *testing.T) {
 	res := &http.Response{
@@ -42,12 +34,6 @@ func TestXMLHandle(t *testing.T) {
 		StatusCode: 200,
 		Body:       testutil.GetAwsEc2ListMultiResponseReader(),
 	}
-	// s := NewSchema(openapi3.NewSchema(), "")
-	// pr, err := s.ProcessHttpResponse(res, "")
-	// assert.NilError(t, err)
-	// assert.Assert(t, pr != nil)
-
-	// fr := getFileRoot(t)
 
 	b, err := GetServiceDocBytes(fmt.Sprintf("aws/%s/services/ec2.yaml", "v0.1.0"))
 	if err != nil {
@@ -62,11 +48,6 @@ func TestXMLHandle(t *testing.T) {
 	assert.Assert(t, svc != nil)
 
 	assert.Equal(t, svc.GetName(), "ec2")
-
-	// l := openapi3.NewLoader()
-	// svc, err := l.LoadFromFile(filepath.Join(fr, "aws", "v0.1.0", "services", "ec2.yaml"))
-
-	// sc := svc.Components.Schemas["VolumeList"].Value
 
 	rsc, err := svc.GetResource("volumes")
 	assert.NilError(t, err)
@@ -83,12 +64,10 @@ func TestXMLHandle(t *testing.T) {
 
 	// m, err := GetSubObjTyped(getAwsEc2ListMultiResponseReader(), "/DescribeVolumesResponse/volumeSet/item", sc)
 
-	// mc, ok := m.([]map[string]interface{})
-	// assert.Assert(t, ok)
-	// assert.Assert(t, len(mc) == 2)
-	// assert.Assert(t, mc[1]["iops"] == 100)
-	// assert.Assert(t, mc[1]["size"] == 8)
+	mc, ok := processedResponse.([]map[string]interface{})
+	assert.Assert(t, ok)
+	assert.Assert(t, len(mc) == 2)
+	assert.Assert(t, mc[1]["iops"] == 100)
+	assert.Assert(t, mc[1]["size"] == 8)
 
-	// assert.NilError(t, err)
-	// assert.Assert(t, m != nil)
 }
