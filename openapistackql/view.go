4f273aaeff212592d6c7b99cd0508980010895dc
General Kroll <generalkroll0@gmail.com>
2023-01-11 16:20:05 +1100

    sql-backend-predicated-view-support
    Summary:
    
    - Support for SQL backend specialisation of document defined views.
    - Predicate logic future proofing:
        - eg: for different view definitions in different SQL backends.

--- openapistackql/view.go
+++ openapistackql/view.go
@@ -7,7 +7,9 @@ import (
 )
 
 type View struct {
-	DDL string `json:"ddl" yaml:"ddl"`
+	Predicate string `json:"predicate" yaml:"predicate"`
+	DDL       string `json:"ddl" yaml:"ddl"`
+	Fallback  *View  `json:"fallback" yaml:"fallback"` // Future proofing for predicate failover
 }
 
 var _ jsonpointer.JSONPointable = (View)(View{})
@@ -16,6 +18,10 @@ func (qt View) JSONLookup(token string) (interface{}, error) {
 	switch token {
 	case "ddl":
 		return qt.DDL, nil
+	case "predicate":
+		return qt.Predicate, nil
+	case "fallback":
+		return qt.Fallback, nil
 	default:
 		return nil, fmt.Errorf("could not resolve token '%s' from View doc object", token)
 	}
