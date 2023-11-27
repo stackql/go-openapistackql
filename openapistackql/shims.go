609396f5a58ca807462c10fbdfa7cddc7df0d519
General Kroll <generalkroll0@gmail.com>
2023-11-27 21:53:41 +1100

    - Details.

--- openapistackql/shims.go
+++ openapistackql/shims.go
@@ -25,8 +25,8 @@ func parseRequestBodyParam(k string, v interface{}, s Schema) *requestBodyParam 
 			if s != nil {
 				isStringRestrictedRaw, hasStr := s.getExtension(ExtensionKeyStringOnly)
 				if hasStr {
-					boolStr, isBoolStr := isStringRestrictedRaw.(string)
-					if isBoolStr && boolStr == "true" {
+					boolBytes, isBoolStr := isStringRestrictedRaw.([]byte)
+					if isBoolStr && string(boolBytes) == "true" {
 						isStringRestricted = true
 					}
 				}
